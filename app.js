342adcecffe2053538c2303501f11a2dc6930594
uzaimaoffical <uzaimabari2004@gmail.com>
2024-10-19 13:45:44 -0700

    work

--- app.js
+++ app.js
@@ -34,13 +34,12 @@ function displayData(post) {
 
     console.log(post);
   });
-
-
-  
 }
 // ======createpost====
 
-document.getElementById("createPostForm").addEventListener("submit", function (e) {
+document
+  .getElementById("createPostForm")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
     const name = document.getElementById("name").value;
     const tittle = document.getElementById("title").value;
@@ -57,21 +56,21 @@ document.getElementById("createPostForm").addEventListener("submit", function (e
     console.log(newPost);
 
     fetch(apiUrl, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newPost),
     })
       .then((response) => response.json())
       .then((data) => console.log(data))
       .catch((error) => console.log(error));
+    document.getElementById("name").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("avatar").value = "";
+    document.getElementById("body").value = "";
   });
- document.getElementById("name").value="";
- document.getElementById("title").value="";
- document.getElementById("avatar").value="";
- document.getElementById("body").value="";
-  
+
 // ================DELET==========================
 
 function deletePost(id) {
@@ -82,7 +81,6 @@ function deletePost(id) {
       if (response.ok) {
         return response.json();
         console.log(response);
-        
       }
     })
     .then((data) => {
@@ -90,65 +88,60 @@ function deletePost(id) {
       alert(`${data.name}(successfully deleted`);
       fetchPost();
     })
-   
+
     .catch((error) => console.log(`error`, error));
 }
 // ==========edit post========
-function editPost(id){
+function editPost(id) {
   fetch(`${apiUrl}/${id}`)
-  .then((response)=>response.json())
-  .then((post)=>{
-    console.log("post ",post);
-document.getElementById("create-post").style.display="none"
-document.getElementById("update-post").style.display="block"
-    document.getElementById("updatepostForm").name.value=post.name;
-document.getElementById("updatepostForm").tittle.value =post.tittle
-document.getElementById("updatepostForm").avatar.value =post.avatar
-document.getElementById("updatepostForm").body.value =post.body
-
-
-
-})
-  .catch((error) => console.log(`error`, error));
-  document.getElementById("updatepostForm").addEventListener("submit",function(e){
-    e.preventDefault()
-    console.log("hello i am raning");
-    const name = document.getElementById("name").value;
+    .then((response) => response.json())
+    .then((post) => {
+      console.log("post ", post);
+      document.getElementById("create-post").style.display = "none";
+      document.getElementById("update-post").style.display = "block";
+      document.getElementById("updatepostForm").name.value = post.name;
+      document.getElementById("updatepostForm").tittle.value = post.tittle;
+      document.getElementById("updatepostForm").avatar.value = post.avatar;
+      document.getElementById("updatepostForm").body.value = post.body;
+    })
+    .catch((error) => console.log(`error`, error));
+  document
+    .getElementById("updatepostForm")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      // console.log("hello i am raning");
+      const name = document.getElementById("name").value;
       const tittle = document.getElementById("title").value;
       const avatar = document.getElementById("avatar").value;
       const body = document.getElementById("body").value;
-      const ubdateData = {
+      const updateData = {
         name: name,
         tittle: tittle,
         avatar: avatar,
         body: body,
         createdAt: new Date().toISOString,
       };
-  
+
       fetch(`${apiUrl}/${id}`, {
         method: "PUT",
-        Headers: {
+        headers: {
           "content-Type": "application/json",
         },
-        body: JSON.stringify(ubdateData),
+        body: JSON.stringify(updateData),
       })
         .then((response) => response.json())
-        .then((data) =>{
-          alert(`${data.name} succesfully update`
-          )
-          fetchPost()
-document.getElementById("create-post").style.display="block"
-document.getElementById("update-post").style.display="none"
+        .then((data) => {
+          alert(`${data.name} succesfully update`);
 
-        }
-        
-      )
+          fetchPost();
+          document.getElementById("create-post").style.display = "block";
+          document.getElementById("update-post").style.display = "none";
+          console.log(data);
+        })
         .catch((error) => console.log(error));
-  
-        console.log("data",name,tittle,avatar,body);
+
+      
     });
-    
-  
 }
 // ===========ubdate api call========
 // document.getElementById("ubdatepostForm").addEventListener("submit",function(e){
@@ -179,4 +172,3 @@ document.getElementById("update-post").style.display="none"
 
 //       console.log("data",name,tittle,avatar,description);
 //   });
-  
